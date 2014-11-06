d13e8b9ca07f55895066b176395a2c898db5d2ec
almgohar <almgohar@gmail.com>
2014-11-07 00:45:28 +0300

    add question & answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,15 @@
 class Answer < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :question
+  validates_presence_of :user
+  validates_presence_of :question
+
+  def get_rating()
+    @answer_rating = AnswerRating.find(:all, :conditions => {:answer_id => self.id})
+    @answer_rating.each do |rating|
+      @sum += rating;
+    end
+    @average = @sum/@answer_rating.size 
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
 end

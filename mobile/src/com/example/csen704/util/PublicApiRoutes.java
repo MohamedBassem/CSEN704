b907fb4f551fe0df2606ea8083f88d37e409aee6
MohamedBassem <medoox240@gmail.com>
2014-12-20 17:10:27 +0200

    Adding register

--- mobile/src/com/example/csen704/util/PublicApiRoutes.java
+++ mobile/src/com/example/csen704/util/PublicApiRoutes.java
@@ -1,15 +1,24 @@
 package com.example.csen704.util;
 
 
-import com.example.csen704.model.User;
-
 import retrofit.Callback;
-import retrofit.http.*;
+import retrofit.client.Response;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+import com.example.csen704.model.User;
 
 public interface PublicApiRoutes {
 
 	@POST("/sessions")
 	@FormUrlEncoded
 	void login(@Field("email") String email, @Field("password") String password, Callback<User> callback);
+
+	@POST("/users")
+	@FormUrlEncoded
+	void register(@Field("email") String email, @Field("name") String name, @Field("password") String password, Callback<Response> callback);
+
+
 
 }
